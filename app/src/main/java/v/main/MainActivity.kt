ddef216e11d8e4119ed31868a09fdba25589cb8c
xiaoshan <operatium@163.com>
2018-06-01 08:43:17 +0800

    webview

--- app/src/main/java/v/main/MainActivity.kt
+++ app/src/main/java/v/main/MainActivity.kt
@@ -2,9 +2,18 @@ package v.main
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 import com.yyx.R
 import kotlinx.android.synthetic.main.main_activity_main.*
 import other.base.BaseActivity
+import android.widget.Toast
+import android.webkit.JavascriptInterface
+import v.main.MainActivity.JsInteraction
+
+
+
+
 
 /**
  * Created by Administrator on 2018/5/31.
@@ -27,11 +36,20 @@ class MainActivity : BaseActivity() {
     }
 
     fun loadBackgroundImage(){
-        val webSettings = background1.settings
-        // 设置与Js交互的权限
-        webSettings.javaScriptEnabled = true
-        // 设置允许JS弹窗
-        webSettings.javaScriptCanOpenWindowsAutomatically = true
+
+        background1.getSettings().setJavaScriptEnabled(true)
+        background1.getSettings().setJavaScriptCanOpenWindowsAutomatically(true)
+        background1.getSettings().setSupportMultipleWindows(true)
+        background1.setWebViewClient(WebViewClient())
+        background1.setWebChromeClient(WebChromeClient())
         background1.loadUrl("https://source.unsplash.com/random")
+        background1.addJavascriptInterface(JsInteraction(), "control")
+    }
+
+    inner class JsInteraction {
+        @JavascriptInterface
+        fun toastMessage(message: String) {   //提供给js调用的方法
+            Toast.makeText(applicationContext, message, Toast.LENGTH_LONG).show()
+        }
     }
 }
